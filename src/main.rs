47909c689e8f0441536c4293464de572627f2441
jackcat13 <christophehenry11@gmail.com>
2025-02-13 14:44:04 +0100

    feat: Scene as function pointer and print coding space on the right

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::sync::Mutex;
+
 use raylib::{color::Color, prelude::RaylibDraw, RaylibHandle, RaylibThread};
 
 const GAME_NAME: &str = "Coding Survivor";
+static CURRENT_SCENE: Mutex<fn(&mut RaylibHandle, &RaylibThread, i32, i32)> = Mutex::new(main_scene);
 
 fn main() {
     let (mut rl, thread) = raylib::init()
@@ -8,15 +11,18 @@ fn main() {
         .title(GAME_NAME)
         .build();
 
+
     while !rl.window_should_close() {
-        main_scene(&mut rl, &thread);
+        let current_scene = CURRENT_SCENE.lock().expect("Failed to get current scene");
+        let (game_width, game_height) = (rl.get_screen_width(), rl.get_screen_height());
+        current_scene(&mut rl, &thread, game_width, game_height);
     }
 }
 
-fn main_scene(rl: &mut RaylibHandle, thread: &RaylibThread) {
+fn main_scene(rl: &mut RaylibHandle, thread: &RaylibThread, width: i32, height: i32) {
     let mut d = rl.begin_drawing(thread);
-
-    d.clear_background(Color::BLACK);
-    d.draw_text("Hello World", 12, 12, 20, Color::WHITE);
+    
+    d.clear_background(Color::GRAY);
+    d.draw_rectangle(0, 0, width / 3, height, Color::BLACK);
 }
 
